f25b92b79ac03a80a786e41205a249600db18616
amr morgan <amr.morgan@homzmart.com>
2022-10-08 16:26:35 +0200

    [aws] fix bug

--- ubuntu/install-rancher.sh
+++ ubuntu/install-rancher.sh
@@ -18,6 +18,9 @@ cat > $K_CONFIGS/rancher-config.yaml << EOF
 replicas: 1
 EOF
 
+# fix issue
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+
 # inatall rancher
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm install rancher rancher-latest/rancher --debug \
